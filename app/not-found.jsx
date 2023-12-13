ccfae97bb0e7edc5f7fe3f44005b11c17edd3d93
ESE-MONDAY <mondayese8@gmail.com>
2023-12-13 16:55:18 +0100

    Update not-found.jsx

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -4,13 +4,25 @@ import Crying from '../public/Crying.json';
 import Lottie from 'lottie-react';
  
 export default function NotFound() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: Crying,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
   return (
     <div className='w-full max-w-[1440px] h-screen mx-auto text-white bg-[#111111]'>
 
         <main className="flex h-full flex-col items-center justify-center gap-2 px-8 ">
-            <Lottie options={{animationData: Crying, loop: true, autoplay: true, rendererSettings: {preserveAspectRatio: 'xMidYMid slice'}}} height={200} width={200} />
+          
+          <Lottie animationData={Crying} objectFit='contain'
+          style={{width: '100%', height: '40%', scale: '0.7'}}
+                />
+                    
             
-              <p>Oops! . The page you&apos;re looking for might have ventured off on its own adventure</p>
+              <p className='-mt-8'>Oops! . The page you&apos;re looking for might have ventured off on its own adventure</p>
               <Link
                 href="/"
                 className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
